a769d09b57043ec94047e341929b8e4a48d51ecb
Ollie <git@ollie.im>
2023-12-04 18:06:16 +0000

    Initail commit

---
+++ EventHooks.cs
@@ -0,0 +1,88 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Events;
+using CounterStrikeSharp.API.Modules.Utils;
+using CSTimer = CounterStrikeSharp.API.Modules.Timers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CounterStrikeSharp.API.Modules.Timers;
+
+namespace JailbreakPlugin
+{
+    public class EventHooks
+    {
+        private JailbreakPlugin _plugin;
+
+        public EventHooks(JailbreakPlugin plugin)
+        {
+            _plugin = plugin;
+        }
+
+        public void Initialize()
+        {
+            _plugin.RegisterEventHandler<EventPlayerDeath>(PlayerDeathHandler);
+            _plugin.RegisterEventHandler<EventPlayerSpawn>(PlayerSpawnHandler);
+            _plugin.RegisterEventHandler<EventPlayerSpawned>(PlayerSpawnedHandler);
+        }
+
+        private HookResult PlayerSpawnedHandler(EventPlayerSpawned @event, GameEventInfo _)
+        {
+            var player = @event.Userid;
+
+            if (!player.IsValid || !player.PlayerPawn.IsValid || player.IsBot)
+            {
+                Server.PrintToConsole("Nope");
+                return HookResult.Continue;
+            }
+
+            //int to string
+            var teamOne = player.TeamNum.ToString();
+            var teamTwo = player.PendingTeamNum.ToString();
+
+            _plugin.AddTimer(0.5f, () => player.RemoveWeapons());
+
+            Server.PrintToConsole("SPAWNED: " + teamOne + " | " + teamTwo);
+
+            return HookResult.Continue;
+        }
+
+        private HookResult PlayerSpawnHandler(EventPlayerSpawn @event, GameEventInfo _)
+        {
+            var player = @event.Userid;
+
+            if (!player.IsValid || !player.PlayerPawn.IsValid || player.IsBot)
+            {
+                Server.PrintToConsole("Nope2");
+                return HookResult.Continue;
+            }
+
+            //int to string
+            var teamOne = player.TeamNum.ToString();
+            var teamTwo = player.PendingTeamNum.ToString();
+
+            Server.PrintToConsole("SPAWN:" + teamOne + " | " + teamTwo);
+
+            //if T
+
+            _plugin.AddTimer(0.5f, () => player.RemoveWeapons());
+
+            return HookResult.Continue;
+        }
+
+        private HookResult PlayerDeathHandler(EventPlayerDeath @event, GameEventInfo _)
+        {
+            var attacker = @event.Attacker;
+            var victim = @event.Userid;
+            var headshot = @event.Headshot;
+
+            if (attacker.IsValid && victim.IsValid && (attacker != victim) && !attacker.IsBot)
+            {
+            }
+
+            return HookResult.Continue;
+        }
+    }
+}
---
+++ JailbreakPlugin.cs
@@ -0,0 +1,23 @@
+﻿using CounterStrikeSharp.API.Core;
+
+namespace JailbreakPlugin;
+public class JailbreakPlugin : BasePlugin
+{
+    private static JailbreakPlugin _instance;
+    public static JailbreakPlugin Instance => _instance;
+
+    public override string ModuleName => "Dopefish Jailbreak plugin";
+
+    public override string ModuleVersion => "0.0.1";
+
+    private EventHooks _eventHooks;
+
+    public override void Load(bool hotReload)
+    {
+        if (_instance == null) _instance = this;
+        Console.WriteLine("Hello World!");
+
+        _eventHooks = new EventHooks(this);
+        _eventHooks.Initialize();
+    }
+}
